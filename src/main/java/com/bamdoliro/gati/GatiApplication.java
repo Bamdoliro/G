6ade43ff8c968339b9ef3594e9dbf777be19d544
gimhanul <gimhanuly@gmail.com>
2022-03-17 11:13:27 +0900

    SET :: project setting

---
+++ src/main/java/com/bamdoliro/gati/GatiApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.gati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamdoliro/gati/GatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.gati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

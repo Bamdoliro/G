9dab8c2506d57e127786e3dd3f78520f0f6f2ac3
gimhanul <gimhanuly@gmail.com>
2022-04-07 20:53:10 +0900

    ADD :: Exception (#4)
    - Exception 작성
    - ErrorCode 추가

--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -7,7 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "서버에 오류가 발생했습니다."),
-    BAD_REQUEST(400, "잘못된 요청입니다.")
+    BAD_REQUUEUST(400, "잘못된 요청입니다."),
+
+    USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
+    USER_ALREADY_EXISTS(422, "사용자가 이미 존재합니다.")
     ;
 
     private final int status;

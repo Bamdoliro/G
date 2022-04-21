a2e2bfe524c08571a5e337e231a71087fc22cc9c
gimhanul <gimhanuly@gmail.com>
2022-04-21 21:52:51 +0900

    REFACTOR :: typo
    RefreshTOken -> RefreshToken

--- src/main/java/com/bamdoliro/gati/global/security/jwt/JwtProperties.java
+++ src/main/java/com/bamdoliro/gati/global/security/jwt/JwtProperties.java
@@ -4,5 +4,5 @@ public class JwtProperties {
     public static final long ACCESS_TOKEN_VALID_TIME = 30 * 60 * 1000L;
     public static final long REFRESH_TOKEN_VALID_TIME = 30 * 24 * 60 * 60 * 1000L;
     public static final String ACCESS_TOKEN_NAME = "accessToken";
-    public static final String REFRESH_TOKEN_NAME = "refreshTOken";
+    public static final String REFRESH_TOKEN_NAME = "refreshToken";
 }

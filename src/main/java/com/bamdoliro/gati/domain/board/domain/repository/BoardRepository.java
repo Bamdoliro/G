c4c5ca5bf2b5842ebf4f291010a8a3a2ec7753be
leekyukin <leekujin14@gmail.com>
2022-05-09 20:31:56 +0900

    ADD :: 메서드 하나 추가

--- src/main/java/com/bamdoliro/gati/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/bamdoliro/gati/domain/board/domain/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package com.bamdoliro.gati.domain.board.domain.repository;
 
 import com.bamdoliro.gati.domain.board.domain.Board;
+import com.bamdoliro.gati.domain.board.presentation.dto.response.BoardResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.ArrayList;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    ArrayList<Board> findAllByWriter(String writer);
 }

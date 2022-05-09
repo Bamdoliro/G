b6d95c56e19da2020bd6fc892b0cd20c8141323b
leekyukin <leekujin14@gmail.com>
2022-05-09 20:32:25 +0900

    ADD :: savePost, getPostsByWriter

--- src/main/java/com/bamdoliro/gati/domain/board/presentation/BoardController.java
+++ src/main/java/com/bamdoliro/gati/domain/board/presentation/BoardController.java
@@ -1,13 +1,37 @@
 package com.bamdoliro.gati.domain.board.presentation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bamdoliro.gati.domain.board.presentation.dto.request.BoardRequest;
+import com.bamdoliro.gati.domain.board.presentation.dto.response.BoardResponse;
+import com.bamdoliro.gati.domain.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/board")
+@RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
+    private final BoardService boardService;
 
+    @GetMapping("/{writer}")
+    public ArrayList<BoardResponse> getPostsByWriter(
+            @PathVariable String writer
+    ) {
+        return boardService.getPostsByWriter(writer);
+    }
+
+    @PostMapping("/savePost")
+    public BoardResponse posting(
+            @RequestBody @Valid BoardRequest dto
+    ) {
+        log.info(dto.getContent());
+        return boardService.savePost(dto);
+    }
 
 
 
--- src/main/java/com/bamdoliro/gati/domain/board/service/BoardService.java
+++ src/main/java/com/bamdoliro/gati/domain/board/service/BoardService.java
@@ -1,8 +1,16 @@
 package com.bamdoliro.gati.domain.board.service;
 
+import com.bamdoliro.gati.domain.board.domain.Board;
 import com.bamdoliro.gati.domain.board.domain.repository.BoardRepository;
+import com.bamdoliro.gati.domain.board.domain.type.BoardType;
+import com.bamdoliro.gati.domain.board.domain.type.Status;
+import com.bamdoliro.gati.domain.board.presentation.dto.request.BoardRequest;
+import com.bamdoliro.gati.domain.board.presentation.dto.response.BoardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +18,39 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
+    @Transactional
+    public ArrayList<BoardResponse> getPostsByWriter(String writer) {
+        ArrayList<Board> posts = boardRepository.findAllByWriter(writer);
+        ArrayList<BoardResponse> responses = new ArrayList<>();
+        for (Board post : posts) {
+            BoardResponse response = BoardResponse.builder()
+                    .writer(post.getWriter())
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .boardType(post.getBoardType())
+                    .build();
+            responses.add(response);
+        }
+        return responses;
+    }
+
+    @Transactional
+    public BoardResponse savePost(BoardRequest dto) {
+        Board board = Board.builder()
+                .title(dto.getTitle())
+                .writer(dto.getWriter())
+                .content(dto.getContent())
+                .boardType(BoardType.DDO)
+                .status(Status.EXISTED)
+                .build();
+        boardRepository.save(board);
+        BoardResponse response = BoardResponse.builder()
+                .boardType(board.getBoardType())
+                .content(board.getContent())
+                .title(board.getTitle())
+                .writer(board.getWriter())
+                .build();
+
+        return response;
+    }
 }

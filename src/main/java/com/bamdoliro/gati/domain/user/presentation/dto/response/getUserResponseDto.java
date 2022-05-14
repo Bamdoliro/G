73d95e44aec46046175c0b12e87d08de0a2a36b9
gimhanul <gimhanuly@gmail.com>
2022-05-14 09:48:19 +0900

    REFACTOR :: dto 만드는 코드 dto 단으로 이동 (#20)

--- src/main/java/com/bamdoliro/gati/domain/user/presentation/dto/response/getUserResponseDto.java
+++ src/main/java/com/bamdoliro/gati/domain/user/presentation/dto/response/getUserResponseDto.java
@@ -1,5 +1,6 @@
 package com.bamdoliro.gati.domain.user.presentation.dto.response;
 
+import com.bamdoliro.gati.domain.user.domain.User;
 import com.bamdoliro.gati.domain.user.domain.type.Authority;
 import com.bamdoliro.gati.domain.user.domain.type.Gender;
 import com.bamdoliro.gati.domain.user.domain.type.Status;
@@ -18,4 +19,15 @@ public class getUserResponseDto {
     private Gender gender;
     private Authority authority;
     private Status status;
+
+    public static getUserResponseDto fromUser(User user) {
+        return getUserResponseDto.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .birth(user.getBirth())
+                .gender(user.getGender())
+                .authority(user.getAuthority())
+                .status(user.getStatus())
+                .build();
+    }
 }
--- src/main/java/com/bamdoliro/gati/domain/user/service/UserService.java
+++ src/main/java/com/bamdoliro/gati/domain/user/service/UserService.java
@@ -2,8 +2,6 @@ package com.bamdoliro.gati.domain.user.service;
 
 import com.bamdoliro.gati.domain.user.domain.User;
 import com.bamdoliro.gati.domain.user.domain.repository.UserRepository;
-import com.bamdoliro.gati.domain.user.domain.type.Authority;
-import com.bamdoliro.gati.domain.user.domain.type.Status;
 import com.bamdoliro.gati.domain.user.exception.PasswordMismatchException;
 import com.bamdoliro.gati.domain.user.exception.UserAlreadyExistsException;
 import com.bamdoliro.gati.domain.user.exception.UserNotFoundException;
@@ -12,11 +10,9 @@ import com.bamdoliro.gati.domain.user.presentation.dto.response.TokenResponseDto
 import com.bamdoliro.gati.domain.user.presentation.dto.response.getUserResponseDto;
 import com.bamdoliro.gati.global.redis.RedisService;
 import com.bamdoliro.gati.global.security.auth.AuthDetails;
-import com.bamdoliro.gati.global.security.jwt.JwtProperties;
 import com.bamdoliro.gati.global.security.jwt.JwtTokenProvider;
 import com.bamdoliro.gati.global.utils.CookieUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,11 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDate;
 
 import static com.bamdoliro.gati.global.security.jwt.JwtProperties.*;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -42,19 +36,7 @@ public class UserService {
     @Transactional
     public void createUser(CreateUserRequestDto dto) {
         validateCreateUserRequest(dto);
-        userRepository.save(createUserFromCreateUserDto(dto));
-    }
-
-    private User createUserFromCreateUserDto(CreateUserRequestDto dto) {
-        return User.builder()
-                .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .name(dto.getName())
-                .birth(LocalDate.of(dto.getBirthYear(), dto.getBirthMonth(), dto.getBirthDay()))
-                .gender(dto.getGender())
-                .authority(Authority.ROLE_USER)
-                .status(Status.NOT_VERIFIED)
-                .build();
+        userRepository.save(dto.toUser(passwordEncoder.encode(dto.getPassword())));
     }
 
     private void validateCreateUserRequest(CreateUserRequestDto dto) {
@@ -104,14 +86,7 @@ public class UserService {
     public getUserResponseDto getUserInformation() {
         User user = getCurrentUser();
 
-        return getUserResponseDto.builder()
-                .email(user.getEmail())
-                .name(user.getName())
-                .birth(user.getBirth())
-                .gender(user.getGender())
-                .authority(user.getAuthority())
-                .status(user.getStatus())
-                .build();
+        return getUserResponseDto.fromUser(user);
     }
 
     @Transactional

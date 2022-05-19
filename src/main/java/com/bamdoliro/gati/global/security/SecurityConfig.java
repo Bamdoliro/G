54e1d20d4544dd51cf069f09575a2abab9a8cffa
leekyukin <leekujin14@gmail.com>
2022-05-19 11:12:38 +0900

    FIX :: Security 설정 복원

--- src/main/java/com/bamdoliro/gati/global/security/SecurityConfig.java
+++ src/main/java/com/bamdoliro/gati/global/security/SecurityConfig.java
@@ -47,7 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/user/join", "/user/login").permitAll()
-                .antMatchers("/board/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().disable();

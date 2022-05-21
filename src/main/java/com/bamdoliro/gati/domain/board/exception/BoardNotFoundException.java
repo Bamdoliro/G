2571f479a5b88df5c96e8e77909212e168fc62ce
leekyukin <leekujin14@gmail.com>
2022-05-21 21:55:07 +0900

    ADD :: BoardNotFoundException 추가

---
+++ src/main/java/com/bamdoliro/gati/domain/board/exception/BoardNotFoundException.java
@@ -0,0 +1,12 @@
+package com.bamdoliro.gati.domain.board.exception;
+
+import com.bamdoliro.gati.global.error.exception.ErrorCode;
+import com.bamdoliro.gati.global.error.exception.GatiException;
+
+public class BoardNotFoundException extends GatiException {
+    public final static GatiException EXCEPTION = new BoardNotFoundException();
+
+    public BoardNotFoundException() {
+        super(ErrorCode.BOARD_NOT_FOUND);
+    }
+}
--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -13,7 +13,9 @@ public enum ErrorCode {
     USER_ALREADY_EXISTS(422, "사용자가 이미 존재합니다."),
     PASSWORD_MISMATCH(401, "비밀번호가 틀렸습니다."),
 
-    COMMUNITY_NOT_FOUND(404, "공동체를 찾을 수 없습니다.")
+    COMMUNITY_NOT_FOUND(404, "공동체를 찾을 수 없습니다."),
+
+    BOARD_NOT_FOUND(404,"게시물을 찾을 수 없습니다.");
     ;
 
     private final int status;

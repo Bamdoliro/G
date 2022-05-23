680b49cf2f22e2046e50a4e07529ac67c873fa5f
leekyukin <leekujin14@gmail.com>
2022-05-23 21:56:24 +0900

    DELETE :: 불필요한 import 삭제

--- src/main/java/com/bamdoliro/gati/domain/board/presentation/dto/response/BoardResponseDto.java
+++ src/main/java/com/bamdoliro/gati/domain/board/presentation/dto/response/BoardResponseDto.java
@@ -1,13 +1,13 @@
 package com.bamdoliro.gati.domain.board.presentation.dto.response;
 
 import com.bamdoliro.gati.domain.user.domain.User;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
+@Builder
 public class BoardResponseDto {
 
     @NotNull
@@ -16,12 +16,4 @@ public class BoardResponseDto {
     private String content;
     @NotNull
     private User writer;
-
-    @Builder
-    public BoardResponseDto(String title, String content, User writer) {
-        this.title = title;
-        this.content = content;
-        this.writer = writer;
-    }
-
 }

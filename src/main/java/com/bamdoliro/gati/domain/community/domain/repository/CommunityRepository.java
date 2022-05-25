9e7dff034826d2864f8f553e7491de04f96a5cd9
gimhanul <gimhanuly@gmail.com>
2022-05-25 22:11:37 +0900

    ADD :: 이미 존재하는 code인지 검증 (#35)
    - 이미 존재하는 code인지 검증하는 method 추가
    - repository에 관련 method 추가

--- src/main/java/com/bamdoliro/gati/domain/community/domain/repository/CommunityRepository.java
+++ src/main/java/com/bamdoliro/gati/domain/community/domain/repository/CommunityRepository.java
@@ -3,5 +3,9 @@ package com.bamdoliro.gati.domain.community.domain.repository;
 import com.bamdoliro.gati.domain.community.domain.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommunityRepository extends JpaRepository<Community, Long> {
+
+    Optional<Community> findByCode(String code);
 }

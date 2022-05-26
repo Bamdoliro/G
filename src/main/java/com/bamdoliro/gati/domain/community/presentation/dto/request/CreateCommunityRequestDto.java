d310dd794e109021ecb85a475bdc308d42d5d79d
gimhanul <gimhanuly@gmail.com>
2022-05-26 18:09:42 +0900

    ADD :: Private community 생성 logic (#36)
    - Community에 password 추가
    - Community create dto에 password 추가

--- src/main/java/com/bamdoliro/gati/domain/community/presentation/dto/request/CreateCommunityRequestDto.java
+++ src/main/java/com/bamdoliro/gati/domain/community/presentation/dto/request/CreateCommunityRequestDto.java
@@ -3,6 +3,7 @@ package com.bamdoliro.gati.domain.community.presentation.dto.request;
 import com.bamdoliro.gati.domain.community.domain.Community;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -25,6 +26,9 @@ public class CreateCommunityRequestDto {
     @NotNull
     private Boolean isPublic;
 
+    @Nullable
+    private String password;
+
     public Community toEntity(String code) {
         return Community.builder()
                 .name(name)
@@ -32,6 +36,7 @@ public class CreateCommunityRequestDto {
                 .numberOfPeople(numberOfPeople)
                 .code(code)
                 .isPublic(isPublic)
+                .password(password)
                 .build();
     }
 }

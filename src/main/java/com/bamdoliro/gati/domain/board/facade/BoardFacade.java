4ef77dac169c5134b7aa1687c1cf3ffbd35cd047
leekyukin <leekujin14@gmail.com>
2022-05-26 23:22:06 +0900

    ADD :: 삭제된 게시물인지 획인하는 method 추가

--- src/main/java/com/bamdoliro/gati/domain/board/facade/BoardFacade.java
+++ src/main/java/com/bamdoliro/gati/domain/board/facade/BoardFacade.java
@@ -2,7 +2,11 @@ package com.bamdoliro.gati.domain.board.facade;
 
 import com.bamdoliro.gati.domain.board.domain.Board;
 import com.bamdoliro.gati.domain.board.domain.repository.BoardRepository;
+import com.bamdoliro.gati.domain.board.domain.type.Status;
 import com.bamdoliro.gati.domain.board.exception.BoardNotFoundException;
+import com.bamdoliro.gati.domain.community.facade.CommunityFacade;
+import com.bamdoliro.gati.domain.user.domain.User;
+import com.bamdoliro.gati.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +14,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BoardFacade {
 
+
     private final BoardRepository boardRepository;
 
     public Board findBoardById(Long id) {
-        return boardRepository.findById(id)
+        Board board = boardRepository.findById(id)
                 .orElseThrow(() -> BoardNotFoundException.EXCEPTION);
+        if (board.getStatus() == Status.DELETED)
+            throw BoardNotFoundException.EXCEPTION;
+        return board;
+    }
+
+    public Board findMyPosts(User currentUser) {
+        return boardRepository.findByWriter(currentUser);
     }
 }

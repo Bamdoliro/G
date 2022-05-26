872984d286975b676904b9281b5c2f0a5b1cf7f1
leekyukin <leekujin14@gmail.com>
2022-05-26 20:32:28 +0900

    ADD :: Validation 추가

---
+++ src/main/java/com/bamdoliro/gati/domain/board/presentation/dto/request/UpdateBoardRequest.java
@@ -0,0 +1,25 @@
+package com.bamdoliro.gati.domain.board.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Getter
+@AllArgsConstructor
+public class UpdateBoardRequest {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String title;
+
+    @NotNull
+    @Size(min = 10, max = 4000)
+    private String content;
+}

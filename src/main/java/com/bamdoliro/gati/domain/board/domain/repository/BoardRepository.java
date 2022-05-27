fa22c2e93a38801aa4a663358e787502f42369a1
leekyukin <leekujin14@gmail.com>
2022-05-27 21:44:51 +0900

    DELETE :: 불필요한 import

--- src/main/java/com/bamdoliro/gati/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/bamdoliro/gati/domain/board/domain/repository/BoardRepository.java
@@ -4,8 +4,6 @@ import com.bamdoliro.gati.domain.board.domain.Board;
 import com.bamdoliro.gati.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-
 public interface BoardRepository extends JpaRepository<Board, Long> {
     Board findByWriter(User currentUser);
 }

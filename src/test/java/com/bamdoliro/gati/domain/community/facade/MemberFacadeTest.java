0cbd668e3c361eaaee796b6b613362a7e19e861f
gimhanul <gimhanuly@gmail.com>
2022-05-27 17:35:23 +0900

    TEST :: MemberFacadeTest (#31)
    - findMemberById test 성공, 실패 케이스 추가
    - findMemberByUserAndCommunity 성공, 실패 케이스 추가

--- src/test/java/com/bamdoliro/gati/domain/community/facade/MemberFacadeTest.java
+++ src/test/java/com/bamdoliro/gati/domain/community/facade/MemberFacadeTest.java
@@ -4,6 +4,7 @@ import com.bamdoliro.gati.domain.community.domain.Community;
 import com.bamdoliro.gati.domain.community.domain.Member;
 import com.bamdoliro.gati.domain.community.domain.repository.MemberRepository;
 import com.bamdoliro.gati.domain.community.domain.type.Authority;
+import com.bamdoliro.gati.domain.community.exception.MemberNotFoundException;
 import com.bamdoliro.gati.domain.community.exception.UserNotCommunityMemberException;
 import com.bamdoliro.gati.domain.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
@@ -13,10 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -47,6 +50,62 @@ class MemberFacadeTest {
             .authority(Authority.MEMBER)
             .build();
 
+    @DisplayName("[Facade] findMemberById")
+    @Test
+    void givenMemberId_whenFindingMember_thenReturnsMember() {
+        // given
+        given(memberRepository.findById(any())).willReturn(Optional.of(defaultMember));
+
+        // when
+        Member member = memberFacade.findMemberById(defaultMember.getId());
+
+        // then
+        verify(memberRepository, times(1)).findById(any());
+        assertEquals(defaultMember.getCommunity(), member.getCommunity());
+        assertEquals(defaultMember.getUser(), member.getUser());
+        assertEquals(defaultMember.getAuthority(), member.getAuthority());
+    }
+
+    @DisplayName("[Facade] findMemberById - member 가 없는 경우")
+    @Test
+    void givenInvalidMemberId_whenFindingMember_thenThrowsMemberNotFoundException() {
+        // given
+        given(memberRepository.findById(any())).willReturn(Optional.empty());
+
+        // when and then
+        assertThrows(MemberNotFoundException.class, () ->
+                memberFacade.findMemberById(defaultMember.getId()));
+        verify(memberRepository, times(1)).findById(any());
+    }
+
+    @DisplayName("[Facade] findMemberByUserAndCommunity")
+    @Test
+    void givenUserAndCommunity_whenFindingMember_thenReturnsMember() {
+        // given
+        given(memberRepository.findByUserAndCommunity(any(), any())).willReturn(Optional.of(defaultMember));
+
+        // when
+        Member member = memberFacade.findMemberByUserAndCommunity(defaultUser, defaultCommunity);
+
+        // then
+        verify(memberRepository, times(1)).findByUserAndCommunity(any(), any());
+        assertEquals(defaultMember.getUser(), member.getUser());
+        assertEquals(defaultMember.getCommunity(), member.getCommunity());
+        assertEquals(defaultMember.getAuthority(), member.getAuthority());
+    }
+
+    @DisplayName("[Facade] findMemberByUserAndCommunity - community 에 가입한 user 가 아닌 경우")
+    @Test
+    void givenInvalidUserAndCommunity_whenFindingMember_thenThrowsUserNotCommunityMemberException() {
+        // given
+        given(memberRepository.findByUserAndCommunity(any(), any())).willReturn(Optional.empty());
+
+        // when and then
+        assertThrows(UserNotCommunityMemberException.class, () ->
+                memberFacade.findMemberByUserAndCommunity(defaultUser, defaultCommunity));
+        verify(memberRepository, times(1)).findByUserAndCommunity(any(), any());
+    }
+
     @DisplayName("[Facade] check member")
     @Test
     void givenUserAndCommunity_whenCheckingMember_thenChecksSuccessfully() {
@@ -60,7 +119,7 @@ class MemberFacadeTest {
         verify(memberRepository, times(1)).existsByUserAndCommunity(defaultUser, defaultCommunity);
     }
 
-    @DisplayName("[Facade] check member - community member가 아닌 경우")
+    @DisplayName("[Facade] check member - community member 가 아닌 경우")
     @Test
     void givenInvalidUserAndCommunity_whenCheckingMember_thenThrowsUserNotCommunityMemberException() {
         // given

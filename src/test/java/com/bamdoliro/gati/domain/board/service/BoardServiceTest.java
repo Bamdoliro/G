51c5c6c432b6e9ac109646653a772458dc3b1d4d
leekyukin <leekujin14@gmail.com>
2022-06-12 16:27:16 +0900

    TEST :: board 생성, 디테일 보기
    - 아직 서툴러요 🥲 오류 뜨는 부분 주석 처리 해놨으니까 리뷰 부탁드립니다

--- src/test/java/com/bamdoliro/gati/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/bamdoliro/gati/domain/board/service/BoardServiceTest.java
@@ -8,7 +8,6 @@ import com.bamdoliro.gati.domain.board.presentation.dto.response.BoardDetailDto;
 import com.bamdoliro.gati.domain.community.domain.Community;
 import com.bamdoliro.gati.domain.community.domain.repository.CommunityRepository;
 import com.bamdoliro.gati.domain.community.facade.CommunityFacade;
-import com.bamdoliro.gati.domain.community.facade.MemberFacade;
 import com.bamdoliro.gati.domain.user.domain.User;
 import com.bamdoliro.gati.domain.user.domain.repository.UserRepository;
 import com.bamdoliro.gati.domain.user.domain.type.Authority;
@@ -19,18 +18,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BoardServiceTest {
@@ -41,6 +38,9 @@ class BoardServiceTest {
     @Mock private BoardRepository boardRepository;
     @Mock private UserRepository userRepository;
     @Mock private CommunityRepository communityRepository;
+    @Mock private BoardFacade boardFacade;
+    @Mock private UserFacade userFacade;
+    @Mock private CommunityFacade communityFacade;
 
     Community community = Community.builder()
             .name("우리집")
@@ -66,23 +66,52 @@ class BoardServiceTest {
             .community(community)
             .build();
 
+    @DisplayName("[Service] 게시물 업로드")
     @Test
-    public void test1() throws Exception {
+    public void givenBoard_whenSavePost_thenCreateBoard() throws Exception {
         // given
         given(boardRepository.save(any())).willReturn(board);
-        ArgumentCaptor<Board> captor = ArgumentCaptor.forClass(Board.class);
+        given(userFacade.getCurrentUser()).willReturn(user);
+        given(communityFacade.findCommunityById(any())).willReturn(community);
+        ArgumentCaptor<Board> boardCaptor = ArgumentCaptor.forClass(Board.class);
+        ArgumentCaptor<Long> longCaptor = ArgumentCaptor.forClass(Long.class);
 
         // when
-        userRepository.save(user);
-        communityRepository.save(community);
-        boardRepository.save(board);
+        CreateBoardRequestDto request = new CreateBoardRequestDto(
+                1L,
+                "제목제목제목제목제목젬고젬고제목젬ㄱ조젬고",
+                "내영내영냉용내용내용내용내용ㄴ애욘애요내요랜ㅇ래내요래ㅛ내욘료ㅐㄴ요ㅐ료냐요래"
+        );
+        boardService.savePost(request);
 
         // then
-        verify(boardRepository, times(1)).save(captor.capture());
-        Board savedboard = captor.getValue();
-        assertEquals(user, savedboard.getWriter());
-        assertEquals(community, savedboard.getCommunity());
+        verify(boardRepository, times(1)).save(boardCaptor.capture()); // 여기서 오류남 확인 바람
+        verify(userFacade, times(1)).getCurrentUser();
+        verify(communityFacade, times(1)).findCommunityById(longCaptor.capture());
+
+        Board savedBoard = boardCaptor.getValue();
+
+        assertEquals(1, savedBoard.getCommunity().getId());
+        assertEquals("제목제목제목제목제목젬고젬고제목젬ㄱ조젬고", savedBoard.getTitle());
+        assertEquals("내영내영냉용내용내용내용내용ㄴ애욘애요내요랜ㅇ래내요래ㅛ내욘료ㅐㄴ요ㅐ료냐요래", savedBoard.getContent());
+        assertEquals(user, savedBoard.getWriter());
+        assertEquals(community, savedBoard.getCommunity());
     }
 
-    
+    @DisplayName("[Service] Board 상세 조회")
+    @Test
+    void givenBoardId_whenGetDetailBoard_thenReturnsBoardDetailDto() {
+        // given
+        given(boardFacade.findBoardById(board.getId())).willReturn(board);
+
+        // when
+        BoardDetailDto result = boardService.getDetail(board.getId());
+
+        // then
+        verify(boardFacade, times(1)).findBoardById(board.getId());
+
+        assertEquals(board.getWriter().getName(), result.getWriter());
+        assertEquals(board.getTitle(), result.getTitle());
+        assertEquals(board.getContent(), result.getContent());
+    }
 }

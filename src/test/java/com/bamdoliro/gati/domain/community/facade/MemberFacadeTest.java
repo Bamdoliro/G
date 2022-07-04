e2d32d029bf1493e9cbcedc55dc1a5e15603ec79
leekyukin <leekujin14@gmail.com>
2022-07-04 22:22:50 +0900

    DELETE :: 불필요한 Import

--- src/test/java/com/bamdoliro/gati/domain/community/facade/MemberFacadeTest.java
+++ src/test/java/com/bamdoliro/gati/domain/community/facade/MemberFacadeTest.java
@@ -13,10 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 

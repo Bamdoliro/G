68ae48e1e0904c52b26c43e9f2947863ac0ab7ac
leekyukin <leekujin14@gmail.com>
2022-07-05 22:25:58 +0900

    FIX :: 주석처리 & API url path 변경

--- src/main/java/com/bamdoliro/gati/domain/board/presentation/BoardController.java
+++ src/main/java/com/bamdoliro/gati/domain/board/presentation/BoardController.java
@@ -58,14 +58,16 @@ public class BoardController {
         return boardService.getCommunityPosts(communityId);
     }
 
-    @PutMapping("/like/{boardId}")
+    // 좋아요
+    @PutMapping("/{boardId}/like")
     public void like(
             @PathVariable Long boardId
     ) {
         boardService.like(boardId);
     }
 
-    @DeleteMapping("/cancel-like/{boardId}")
+    // 좋아요 취소
+    @DeleteMapping("/{boardId}/like")
     public void cancelLike(
             @PathVariable Long boardId
     ) {

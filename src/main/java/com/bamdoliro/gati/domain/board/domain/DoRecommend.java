c8799e6e0144905de3d80fb06b30763fed5bf28e
leekyukin <leekujin14@gmail.com>
2022-07-07 22:12:37 +0900

    ADD :: DoRecommend Entity

---
+++ src/main/java/com/bamdoliro/gati/domain/board/domain/DoRecommend.java
@@ -0,0 +1,27 @@
+package com.bamdoliro.gati.domain.board.domain;
+
+import com.bamdoliro.gati.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DoRecommend {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "do_recomend_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "do_post_id")
+    private DoPost doPost;
+
+}

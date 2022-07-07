63ba9f58e0149091cff7b3861ea1b927c691cfc1
gimhanul <gimhanuly@gmail.com>
2022-07-07 17:48:50 +0900

    REFACTOR :: RequerstParam -> PathVariable (#79)

--- src/main/java/com/bamdoliro/gati/domain/chat/presentation/RoomMemberController.java
+++ src/main/java/com/bamdoliro/gati/domain/chat/presentation/RoomMemberController.java
@@ -2,10 +2,7 @@ package com.bamdoliro.gati.domain.chat.presentation;
 
 import com.bamdoliro.gati.domain.chat.service.RoomMemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat/room/member")
@@ -14,8 +11,8 @@ public class RoomMemberController {
 
     private final RoomMemberService roomMemberService;
 
-    @PostMapping("/join")
-    public void joinRoom(@RequestParam Long roomId) {
+    @PostMapping("/join/{roomId}")
+    public void joinRoom(@PathVariable Long roomId) {
         roomMemberService.joinRoom(roomId);
     }
 }

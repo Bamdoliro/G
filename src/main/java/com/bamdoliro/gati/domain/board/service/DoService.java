8acc3a05d399ffd5777d15c9a6bf89e7504aa411
leekyukin <leekujin14@gmail.com>
2022-07-08 11:25:32 +0900

    ADD :: do 게시물 포트팅 Logic

--- src/main/java/com/bamdoliro/gati/domain/board/service/DoService.java
+++ src/main/java/com/bamdoliro/gati/domain/board/service/DoService.java
@@ -1,13 +1,28 @@
 package com.bamdoliro.gati.domain.board.service;
 
+import com.bamdoliro.gati.domain.board.domain.DoPost;
 import com.bamdoliro.gati.domain.board.domain.repository.DoRepository;
+import com.bamdoliro.gati.domain.board.presentation.dto.request.CreateDoRequestDto;
+import com.bamdoliro.gati.domain.user.domain.User;
+import com.bamdoliro.gati.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DoService {
 
     private final DoRepository doRepository;
+    private final UserFacade userFacade;
 
+    @Transactional
+    public void post(CreateDoRequestDto request) {
+        User user = userFacade.getCurrentUser();
+        DoPost doPost = request.toEntity();
+        doPost.setRelation(user);
+
+        doRepository.save(doPost);
+    }
+    
 }

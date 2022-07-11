47556727cf650ff974dfca45bdc36d203d021911
leekyukin <leekujin14@gmail.com>
2022-07-12 08:17:44 +0900

    FIX :: change class name

--- src/main/java/com/bamdoliro/gati/domain/board/domain/repository/DoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bamdoliro.gati.domain.board.domain.repository;
-
-import com.bamdoliro.gati.domain.board.domain.DoPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoRepository extends JpaRepository<DoPost, Long> {
-}

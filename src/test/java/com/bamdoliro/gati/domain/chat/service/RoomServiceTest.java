1c2e558156a85517d00de38bb007d6957a18c446
gimhanul <gimhanuly@gmail.com>
2022-07-13 18:47:58 +0900

    TEST :: update Room (#76)
    - update room logic test

--- src/test/java/com/bamdoliro/gati/domain/chat/service/RoomServiceTest.java
+++ src/test/java/com/bamdoliro/gati/domain/chat/service/RoomServiceTest.java
@@ -4,6 +4,7 @@ import com.bamdoliro.gati.domain.chat.domain.RoomMember;
 import com.bamdoliro.gati.domain.chat.domain.Message;
 import com.bamdoliro.gati.domain.chat.domain.Room;
 import com.bamdoliro.gati.domain.chat.domain.repository.RoomRepository;
+import com.bamdoliro.gati.domain.chat.facade.RoomFacade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,6 +31,9 @@ class RoomServiceTest {
     @Mock
     private RoomRepository roomRepository;
 
+    @Mock
+    private RoomFacade roomFacade;
+
     private final Room defaultRoom = Room.builder()
             .name("귀요미팟")
             .build();
@@ -50,5 +54,18 @@ class RoomServiceTest {
         assertEquals("귀요미팟", savedRoom.getName());
         assertEquals(new ArrayList<RoomMember>(), savedRoom.getMembers());
         assertEquals(new ArrayList<Message>(), savedRoom.getMessages());
+    }
+
+    @Test
+    @DisplayName("[Service] Room 정보 수정")
+    void givenRoomIdAndUpdateName_whenUpdatingRoom_thenUpdatesRoomName() {
+        // given
+        given(roomFacade.findRoomById(any())).willReturn(defaultRoom);
+
+        // when
+        roomService.updateRoom(defaultRoom.getId(), "변경된 이름");
+
+        // then
+        assertEquals("변경된 이름", defaultRoom.getName());
     }
 }

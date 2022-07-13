67a4920cb3d3cd073b66f10780c9ed98b811b1da
gimhanul <gimhanuly@gmail.com>
2022-07-13 10:00:45 +0900

    ADD :: Room 나가는 logic (#72)
    - room 찾는 facade 추가
    - room member 삭제하는 나가기 service 추가
    - Repository에 room 찾는 거 추가

--- src/main/java/com/bamdoliro/gati/domain/chat/domain/repository/RoomMemberRepository.java
+++ src/main/java/com/bamdoliro/gati/domain/chat/domain/repository/RoomMemberRepository.java
@@ -1,7 +1,13 @@
 package com.bamdoliro.gati.domain.chat.domain.repository;
 
+import com.bamdoliro.gati.domain.chat.domain.Room;
 import com.bamdoliro.gati.domain.chat.domain.RoomMember;
+import com.bamdoliro.gati.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RoomMemberRepository extends CrudRepository<RoomMember, Long> {
+
+    Optional<RoomMember> findByRoomAndUser(Room room, User user);
 }

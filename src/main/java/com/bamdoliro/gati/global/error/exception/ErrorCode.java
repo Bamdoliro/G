1f185073fa04ad19853e824b05576f1f1d9927e3
gimhanul <gimhanuly@gmail.com>
2022-07-24 23:37:57 +0900

    ADD :: ExpiredAuthException (#87)
    - refresh token 만료됐을때 날리는 exception 정의

--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -13,6 +13,8 @@ public enum ErrorCode {
     USER_ALREADY_EXISTS(422, "사용자가 이미 존재합니다."),
     PASSWORD_MISMATCH(401, "비밀번호가 틀렸습니다."),
 
+    EXPIRED_AUTH(403, "만료된 사용자입니다."),
+
     COMMUNITY_NOT_FOUND(404, "공동체를 찾을 수 없습니다."),
     COMMUNITY_PASSWORD_MISMATCH(401, "커뮤니티 비밀번호가 틀렸습니다."),
     USER_NOT_COMMUNITY_MEMBER(422, "사용자가 해당 커뮤니티 멤버가 아닙니다."),

3e810fcc917086a200dbe61a965f452beb11d10c
leekyukin <leekujin14@gmail.com>
2022-07-30 21:48:18 +0900

    FIX :: LikeRepository 변수명

--- src/main/java/com/bamdoliro/gati/domain/board/facade/LikeFacade.java
+++ src/main/java/com/bamdoliro/gati/domain/board/facade/LikeFacade.java
@@ -5,17 +5,18 @@ import com.bamdoliro.gati.domain.board.domain.repository.LikeRepository;
 import com.bamdoliro.gati.domain.board.exception.LikeOverlapException;
 import com.bamdoliro.gati.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.internal.predicate.LikePredicate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class LikeFacade {
 
-    private final LikeRepository boardLikeRepository;
+    private final LikeRepository likeRepository;
 
     // 좋아요 충복 체크
     public void validateLike(Board board, User user) {
-        if (boardLikeRepository.existsByBoardAndLiker(board, user)) {
+        if (likeRepository.existsByBoardAndLiker(board, user)) {
             throw LikeOverlapException.EXCEPTION;
         }
     }

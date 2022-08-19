d8c446406af404d5c19b8b5e37319663744e8fc0
gimhanul <gimhanuly@gmail.com>
2022-08-19 15:00:10 +0900

    ADD :: room member service (#71, #72)
    - room member 만들 떄 socket client 바름
    - 버거웠다...
    - postman으로 테스트도 함 ㅎ

--- src/main/java/com/bamdoliro/gati/domain/chat/presentation/RoomMemberController.java
+++ src/main/java/com/bamdoliro/gati/domain/chat/presentation/RoomMemberController.java
@@ -1,26 +1,28 @@
 package com.bamdoliro.gati.domain.chat.presentation;
 
+import com.bamdoliro.gati.domain.chat.presentation.dto.request.RoomRequestDto;
 import com.bamdoliro.gati.domain.chat.service.RoomMemberService;
 import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.annotation.OnEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
-@RestController
-@RequestMapping("/chat/room/{roomId}/member")
+@Controller
 @RequiredArgsConstructor
 public class RoomMemberController {
 
     private final RoomMemberService roomMemberService;
 
-    @PostMapping
-    public void joinRoom(SocketIOClient client, @PathVariable Long roomId) {
-        roomMemberService.joinRoom(client, roomId);
+    @OnEvent("join-room")
+    public void joinRoom(SocketIOClient client, @RequestBody @Valid RoomRequestDto request) {
+        roomMemberService.joinRoom(client, request);
     }
 
-    @DeleteMapping
-    public void leaveRoom(SocketIOClient client, @PathVariable Long roomId) {
-        roomMemberService.leaveRoom(client, roomId);
+    @OnEvent("leave-room")
+    public void leaveRoom(SocketIOClient client, @RequestBody @Valid RoomRequestDto request) {
+        roomMemberService.leaveRoom(client, request);
     }
 }
--- src/main/java/com/bamdoliro/gati/domain/chat/service/RoomMemberService.java
+++ src/main/java/com/bamdoliro/gati/domain/chat/service/RoomMemberService.java
@@ -5,6 +5,7 @@ import com.bamdoliro.gati.domain.chat.domain.RoomMember;
 import com.bamdoliro.gati.domain.chat.domain.repository.RoomMemberRepository;
 import com.bamdoliro.gati.domain.chat.facade.RoomFacade;
 import com.bamdoliro.gati.domain.chat.facade.RoomMemberFacade;
+import com.bamdoliro.gati.domain.chat.presentation.dto.request.RoomRequestDto;
 import com.bamdoliro.gati.domain.user.domain.User;
 import com.bamdoliro.gati.domain.user.facade.UserFacade;
 import com.corundumstudio.socketio.SocketIOClient;
@@ -22,8 +23,8 @@ public class RoomMemberService {
     private final UserFacade userFacade;
 
     @Transactional
-    public void joinRoom(SocketIOClient client, Long roomId) {
-        joinRoom(client, roomFacade.findRoomById(roomId), userFacade.getCurrentUser());
+    public void joinRoom(SocketIOClient client, RoomRequestDto request) {
+        joinRoom(client, roomFacade.findRoomById(Long.valueOf(request.getRoomId())), userFacade.findUserByClient(client));
     }
 
     @Transactional
@@ -39,12 +40,12 @@ public class RoomMemberService {
     }
 
     @Transactional
-    public void leaveRoom(SocketIOClient client, Long roomId) {
-        client.leaveRoom(String.valueOf(roomId));
+    public void leaveRoom(SocketIOClient client, RoomRequestDto request) {
+        client.leaveRoom(request.getRoomId());
 
         roomMemberRepository.delete(
                 roomMemberFacade.findRoomMemberByRoomAndUser(
-                        roomFacade.findRoomById(roomId), userFacade.getCurrentUser()
+                        roomFacade.findRoomById(Long.valueOf(request.getRoomId())), userFacade.findUserByClient(client)
                 )
         );
     }

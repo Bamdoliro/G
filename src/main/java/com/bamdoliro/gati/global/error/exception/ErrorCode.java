e1598534a994a1ffe462e8058901da8ad30c6930
leekyukin <leekujin14@gmail.com>
2022-09-02 22:52:40 +0900

    STYLE

--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -31,7 +31,7 @@ public enum ErrorCode {
 
     DDO_NOT_FOUND(404, "뚜를 찾을 수 없습니다."),
     ALREADY_JOIN(422, "이미 참여중입니다."),
-    EXCESS_OF_CAPACITY(422, "정원이 초과되었습니다.");
+    EXCESS_OF_CAPACITY(422, "정원이 초과되었습니다."),
 
     ;
 

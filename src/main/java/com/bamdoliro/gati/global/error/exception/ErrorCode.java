aa7037ef07c30538ff2f9574c9a5f87bd23d92f7
leekyukin <leekujin14@gmail.com>
2022-09-02 22:53:13 +0900

    ADD :: 뚜 추천 & 뚜 추천 취소

--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -33,6 +33,8 @@ public enum ErrorCode {
     ALREADY_JOIN(422, "이미 참여중입니다."),
     EXCESS_OF_CAPACITY(422, "정원이 초과되었습니다."),
 
+    RECOMMENDATION_NOT_FOUND(404, "추천한적이 없습니다."),
+    ALREADY_RECOMMEND(422, "이미 추천중입니다."),
     ;
 
     private final int status;

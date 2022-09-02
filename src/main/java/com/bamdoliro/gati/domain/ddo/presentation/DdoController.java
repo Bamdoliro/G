2cd9e0fde4fd4b9635b592fd6ac0331c3e317eb3
leekyukin <leekujin14@gmail.com>
2022-09-02 22:50:47 +0900

    ADD :: 추천 순으로 뚜 조회하기

--- src/main/java/com/bamdoliro/gati/domain/ddo/presentation/DdoController.java
+++ src/main/java/com/bamdoliro/gati/domain/ddo/presentation/DdoController.java
@@ -2,9 +2,16 @@ package com.bamdoliro.gati.domain.ddo.presentation;
 
 import com.bamdoliro.gati.domain.ddo.presentation.dto.request.CreateDdoRequestDto;
 import com.bamdoliro.gati.domain.ddo.presentation.dto.response.DdoDetailResponseDto;
+import com.bamdoliro.gati.domain.ddo.presentation.dto.response.DdoResponseDto;
 import com.bamdoliro.gati.domain.ddo.service.DdoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ddo")
@@ -20,11 +27,17 @@ public class DdoController {
         ddoService.savePost(request);
     }
 
+    @GetMapping("/community/{communityId}")
+    public List<DdoResponseDto> getDdoOrderByRecommendation(
+            @PathVariable Long communityId
+    ) {
+        return ddoService.findDdoOrderByRecommendationSize(communityId);
+    }
+
     @GetMapping("/{id}")
     public DdoDetailResponseDto getDoDetail(
             @PathVariable(name = "id") Long id
     ) {
         return ddoService.getDdoDetail(id);
     }
-
 }

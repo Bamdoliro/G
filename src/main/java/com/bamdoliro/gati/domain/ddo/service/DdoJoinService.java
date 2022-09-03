0bd1d0b33c03b0c0fbf95c612e9dd34d85c3c218
leekyukin <leekujin14@gmail.com>
2022-09-03 16:00:09 +0900

    ADD :: DDO에 참여한 참여자 리스트 조회

--- src/main/java/com/bamdoliro/gati/domain/ddo/service/DdoJoinService.java
+++ src/main/java/com/bamdoliro/gati/domain/ddo/service/DdoJoinService.java
@@ -5,11 +5,17 @@ import com.bamdoliro.gati.domain.ddo.domain.Ddo;
 import com.bamdoliro.gati.domain.ddo.domain.DdoJoin;
 import com.bamdoliro.gati.domain.ddo.domain.repository.DdoJoinRepository;
 import com.bamdoliro.gati.domain.ddo.facade.DdoFacade;
+import com.bamdoliro.gati.domain.ddo.presentation.dto.response.UserResponseDto;
 import com.bamdoliro.gati.domain.user.domain.User;
 import com.bamdoliro.gati.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +35,14 @@ public class DdoJoinService {
         ddoFacade.validationJoin(ddo, user);
 
         ddoJoinRepository.save(DdoJoin.createDoJoin(ddo, user));
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> findJoiners(Long id) {
+        Ddo ddo = ddoFacade.findDdoById(id);
+        return ddo.getDdoJoinList().stream()
+                .map(DdoJoin::getJoiner)
+                .map(UserResponseDto::of)
+                .collect(Collectors.toList());
     }
 }

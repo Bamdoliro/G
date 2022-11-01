35fffaf5b93d225bede7f2c6f0e55153f6d2356b
gimhanul <gimhanuly@gmail.com>
2022-11-02 08:20:09 +0900

    REFACTOR :: DdoErrorProperty (#143)
    - ddo error property 추가

--- src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/gati/global/error/exception/ErrorCode.java
@@ -15,17 +15,6 @@ public enum ErrorCode implements ErrorProperty {
 
     EXPIRED_TOKEN(401, "만료된 토큰입니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
-
-
-
-
-
-    DDO_NOT_FOUND(404, "뚜를 찾을 수 없습니다."),
-    ALREADY_JOIN(422, "이미 참여중입니다."),
-    EXCESS_OF_CAPACITY(422, "정원이 초과되었습니다."),
-
-    RECOMMENDATION_NOT_FOUND(404, "추천한적이 없습니다."),
-    ALREADY_RECOMMEND(422, "이미 추천중입니다."),
     ;
 
     private final int status;

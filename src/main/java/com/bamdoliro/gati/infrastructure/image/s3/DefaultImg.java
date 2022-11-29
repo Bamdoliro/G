a6110041c76a57ea2fe5cbb9e24e901b211b11e1
leekyukin <leekujin14@gmail.com>
2022-11-29 11:45:49 +0900

    FIX :: 기본 프로필 이미지 변경

--- src/main/java/com/bamdoliro/gati/infrastructure/image/s3/DefaultImg.java
+++ src/main/java/com/bamdoliro/gati/infrastructure/image/s3/DefaultImg.java
@@ -1,6 +1,6 @@
 package com.bamdoliro.gati.infrastructure.image.s3;
 
 public class DefaultImg {
-    public static final String USER = "https://gati-bucket.s3.ap-northeast-2.amazonaws.com/gati-bucket/dc1a5bc8-02ac-4031-95dc-8f1408d22395-%E1%84%90%E1%85%A2%E1%84%80%E1%85%A7%E1%86%BC%E1%84%87%E1%85%A7%E1%86%BC%E1%84%89%E1%85%B5%E1%86%AB.jpeg";
+    public static final String USER = "https://gati-bucket.s3.ap-northeast-2.amazonaws.com/gati-bucket/e266b003-5583-4ca0-aa3e-dab28f862dcc-default-img.png";
     public static final String COMMUNITY = "https://gati-bucket.s3.ap-northeast-2.amazonaws.com/gati-bucket/cb557d2b-a505-4097-92ff-e53b02719da9-90b4ce.png";
 }
